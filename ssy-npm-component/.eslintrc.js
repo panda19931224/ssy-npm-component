77b244f883b77dfdd9eb229e2ac5354f6e7d266b
panda19931224 <18805152470@163.com>
2022-08-21 21:45:48 +0800

    sixthss

--- ssy-npm-component/.eslintrc.js
+++ ssy-npm-component/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -69,7 +69,7 @@ module.exports = {
       "editor.defaultFormatter": "rvest.vs-code-prettier-eslint"
     },
     "editor.codeActionsOnSave": {
-      "source.fixAll.eslint": true
+      // "source.fixAll.eslint": true
     },
     "bracketPairColorizer.depreciation-notice": false // 两个选择器中是否换行
   }
--- ssy-npm-component/src/pages/testPage/index.vue
+++ ssy-npm-component/src/pages/testPage/index.vue
@@ -15,7 +15,7 @@ export default {
   },
   methods: {
     toLink () {
-      // todo
+      // todos
     }
   }
 }
